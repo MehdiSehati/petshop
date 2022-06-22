e6285d58271ed23b710dffcddc2e64bf1d0efb30
MehdiSehati <sehati.mehdi@gmail.com>
2022-06-22 20:52:53 +0430

    created user and cart sagas for sign in and aign up with google

--- src/pages/Shop/Shop.component.jsx
+++ src/pages/Shop/Shop.component.jsx
@@ -7,13 +7,13 @@ import {
   selectCollectionIsFetching,
   selectIsCollectionsLoaded,
 } from "../../redux/shop/shop.selectors";
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync();
+    const { fetchCollectionsStart } = this.props;
+    fetchCollectionsStart();
   }
   render() {
     const { match } = this.props;
@@ -35,7 +35,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,22 @@
 import { createStore , applyMiddleware} from "redux";
 import logger from 'redux-logger';
 import { persistStore } from "redux-persist"
+import rootSaga from "./root-saga";
+import createSagaMiddleware from 'redux-saga';
 
 import thunk from "redux-thunk";
 
 import rootReducer from "./root-reducer";
+const sagaMiddleware  = createSagaMiddleware();
 
-const middlewares = [logger,thunk];
+
+const middlewares = [logger,sagaMiddleware];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+
+sagaMiddleware.run(rootSaga);
+
 const persistor = persistStore(store);
 
 
